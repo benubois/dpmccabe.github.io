b7f60c129d20683ff82f43c0ca4cb892a0f218e7
Devin McCabe <devin@sandboxindustries.com>
2014-01-06 20:18:32 -0600

    use forked foundation, working asset compilation

--- src/_plugins/asset_pipeline.rb
+++ src/_plugins/asset_pipeline.rb
@@ -1,10 +1,15 @@
 require 'jekyll_asset_pipeline'
 
 module JekyllAssetPipeline
-  # process SCSS files
   class SassConverter < JekyllAssetPipeline::Converter
-    require 'sass'
     require 'compass'
+    require 'zurb-foundation'
+
+    Compass.configuration.sass_dir = 'src/_assets/css'
+
+    Compass.sass_engine_options[:load_paths].each do |path|
+      Sass.load_paths << path
+    end
 
     def self.filetype
       '.scss'
@@ -24,6 +29,18 @@ module JekyllAssetPipeline
 
     def compress
       YUI::CssCompressor.new.compress(@content)
+    end
+  end
+
+  class JavaScriptCompressor < JekyllAssetPipeline::Compressor
+    require 'yui/compressor'
+
+    def self.filetype
+      '.js'
+    end
+
+    def compress
+      return YUI::JavaScriptCompressor.new(munge: true).compress(@content)
     end
   end
 end
