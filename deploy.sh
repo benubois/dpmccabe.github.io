fce6269dfd0a869440ad142fddf3a6b3071a8746
Devin McCabe <devin@sandboxindustries.com>
2014-01-09 19:34:20 -0600

    didn't need to rename src

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 set -e
 bundle exec jekyll build
 git checkout -B master
-rm -rf _src
+rm -rf src
 mv public/* .
 git add -A
-git commit -m "Generated static HTML on $(date)" -a
+git commit -m "Generated static HTML on $(date)"
 git push origin master -f
 git checkout source
