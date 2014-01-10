6a87dc2bd66db56f7d270f3ac38e95498bdcb0de
Devin McCabe <devin@sandboxindustries.com>
2014-01-09 19:32:46 -0600

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 set -e
-git checkout master
-bundle exec jekyll build -t
-rm -rf src
+bundle exec jekyll build
+git checkout -B master
+rm -rf _src
+mv public/* .
+git add -A
 git commit -m "Generated static HTML on $(date)" -a
-git push origin master
+git push origin master -f
 git checkout source
