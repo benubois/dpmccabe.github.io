45e8383fe43cd60577773b3c33d8eb2042a2c04c
Ben Ubois <ben@benubois.com>
2014-01-16 16:10:58 -0800

    Added atom feed with PubSubHubbub.

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,4 @@ git add -A
 git commit -m "Generated static HTML on $(date)"
 git push origin master -f
 git checkout source
+curl -d hub.mode=publish -d hub.url=http://dmcca.be/atom.xml http://pubsubhubbub.appspot.com
